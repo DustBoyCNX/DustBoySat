b6b9b653268d80c03f9498a7b9d94d6683958f28
Nat Weerawan <nat.wrw@gmail.com>
2024-06-28 10:01:40 +0700

    Create merge.sh

---
+++ merge.sh
@@ -0,0 +1,21 @@
+# output from args
+output=$1
+if [ -z "$output" ]; then
+	echo "Error: No output file specified"
+	exit 1
+fi
+board=esp32dev
+# if no dir fw, create it
+if [ ! -d "fw" ]; then
+	mkdir fw
+fi
+# alias esptool.py=esptool
+esptool --chip esp32 merge_bin \
+	-o fw/$output \
+	--flash_mode dio \
+	--flash_freq 40m \
+	--flash_size 4MB \
+	0x1000 .pio/build/$board/bootloader.bin \
+	0x8000 .pio/build/$board/partitions.bin \
+	0x10000 .pio/build/$board/firmware.bin
+ 
